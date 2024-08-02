7784063d7fbba9212e5bf1658f7bfe806564a4ce
BreandenS <162747829+BreandenS@users.noreply.github.com>
2024-08-02 16:38:03 +0200

    re-assigned the print function

--- customersInfo.py
+++ customersInfo.py
@@ -6,21 +6,21 @@ data = pd.read_csv("Infor.csv")
 
 def calculateAverageRating(average_rating):
 
-    print(f"Average Rating: {average_rating}")
-
     return average_rating
-
 
 total_rating = data["Rating"].sum()
 num_feedback = len(data)
 average_rating = total_rating / num_feedback
 
-calculateAverageRating(average_rating)
+average_rating = calculateAverageRating(average_rating)
+print(average_rating)
+
 
 def calculateTop3products(top_3_products):
-    
+
     print(top_3_products)
-    
+
+
 average_ratings = data.groupby("Product")["Rating"].mean()
 sorted_ratings = average_ratings.sort_values(ascending=False)
 
@@ -28,10 +28,12 @@ top_3_products = sorted_ratings.head(3)
 
 calculateTop3products(top_3_products)
 
+
 def calculateFeedback(feedback_per_month):
 
- print(feedback_per_month)
- 
+    print(feedback_per_month)
+
+
 data["Date"] = pd.to_datetime(data["Date"], errors="coerce")
 data["Month"] = data["Date"].dt.to_period("M")
 data = data.dropna(subset=["Month"])
