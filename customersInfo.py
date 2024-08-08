db4bda2f30d2036c42095aa71450967e6a34ba24
BreandenS <162747829+BreandenS@users.noreply.github.com>
2024-08-08 18:22:37 +0200

    Changed and updated the functions

--- customersInfo.py
+++ customersInfo.py
@@ -4,34 +4,27 @@ import pandas as pd
 data = pd.read_csv("Infor.csv")
 
 
+def main():
+
+  
+    total_rating = data["Rating"].sum()
+    num_feedback = len(data)
+    average_rating = total_rating / num_feedback
+    print(calculateAverageRating(average_rating))
+
 def calculateAverageRating(average_rating):
-
+  
     return average_rating
-
-total_rating = data["Rating"].sum()
-num_feedback = len(data)
-average_rating = total_rating / num_feedback
-
-average_rating = calculateAverageRating(average_rating)
-print(average_rating)
-
-
-def calculateTop3products(top_3_products):
-
-    print(top_3_products)
 
 
 average_ratings = data.groupby("Product")["Rating"].mean()
 sorted_ratings = average_ratings.sort_values(ascending=False)
-
 top_3_products = sorted_ratings.head(3)
+print(top_3_products)
 
-calculateTop3products(top_3_products)
 
-
-def calculateFeedback(feedback_per_month):
-
-    print(feedback_per_month)
+def calculateTop3products(top_3_products):
+    return top_3_products
 
 
 data["Date"] = pd.to_datetime(data["Date"], errors="coerce")
@@ -39,4 +32,11 @@ data["Month"] = data["Date"].dt.to_period("M")
 data = data.dropna(subset=["Month"])
 feedback_per_month = data.groupby("Month").size()
 
-calculateFeedback(feedback_per_month)
+print((feedback_per_month))
+
+
+def calculateFeedback(feedback_per_month):
+    return feedback_per_month
+
+
+main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+    
+    total_rating = data["Rating"].sum()
+    num_feedback = len(data)
+    average_rating = total_rating / num_feedback
+    
